f871757ef372c84474defdfb0506cf94840d3768
George Kochera <george.kochera@gmail.com>
2020-08-09 18:36:17 -0400

    game complete, title screen written, keyboard presses mapped correctly, game tested and works according to the rules

--- pearl.py
+++ pearl.py
@@ -11,12 +11,90 @@ import pearl_assets
 WIDTH = pearl_assets.WIDTH
 HEIGHT = pearl_assets.HEIGHT
 
+
+# functions
+def splash_screen():
+
+    # black background
+    bground = pygame.Surface(screen.get_size())
+    bground.fill(pearl_assets.BLACK)
+
+    # press space to begin
+    press_space_text = font_med.render("Press Space to Begin", True, pearl_assets.WHITE)
+    press_space_rect = press_space_text.get_rect()
+    press_space_rect.center = ((WIDTH // 2), (9 * HEIGHT // 10))
+
+    # title
+    title_text = font_med.render("Pearl Puzzle", True, pearl_assets.WHITE, pearl_assets.RED)
+    title_rect = title_text.get_rect()
+    title_rect.center = ((WIDTH // 2), (HEIGHT // 12))
+
+    # instructions
+    base_rect_x, base_rect_y = (WIDTH // 2), ( HEIGHT // 5)
+    reset_text = font_sm.render("Press [SPACE] to reset the puzzle.", True, pearl_assets.WHITE)
+    reset_rect = reset_text.get_rect()
+    reset_rect.center = base_rect_x, base_rect_y
+
+    x_to_quit_text = font_sm.render("Press [x] to quit the puzzle and return to title screen.", True, pearl_assets.WHITE)
+    x_to_quit_rect = x_to_quit_text.get_rect()
+    x_to_quit_rect.center = base_rect_x, base_rect_y + 50
+
+    press_q_to_quit_text = font_sm.render("Press [q] to quit the game.", True, pearl_assets.WHITE)
+    press_q_to_quit_rect = press_q_to_quit_text.get_rect()
+    press_q_to_quit_rect.center = base_rect_x, base_rect_y + 100
+
+    rules_1_text = font_sm.render("You must go through all pearls.", True, pearl_assets.WHITE)
+    rules_1_rect = rules_1_text.get_rect()
+    rules_1_rect.center = base_rect_x, base_rect_y + 200
+
+    rules_2_text = font_sm.render("You must turn before or after a WHITE pearl.", True, pearl_assets.WHITE)
+    rules_2_rect = rules_2_text.get_rect()
+    rules_2_rect.center = base_rect_x, base_rect_y + 230
+
+    rules_3_text = font_sm.render("You must NOT turn before or after a BLACK pearl.", True, pearl_assets.WHITE)
+    rules_3_rect = rules_3_text.get_rect()
+    rules_3_rect.center = base_rect_x, base_rect_y + 260
+
+    rules_4_text = font_sm.render("You must go STRAIGHT through WHITE pearls.", True, pearl_assets.WHITE)
+    rules_4_rect = rules_4_text.get_rect()
+    rules_4_rect.center = base_rect_x, base_rect_y + 290
+
+    rules_5_text = font_sm.render("You must TURN on BLACK pearls.", True, pearl_assets.WHITE)
+    rules_5_rect = rules_5_text.get_rect()
+    rules_5_rect.center = base_rect_x, base_rect_y + 330
+
+    rules_6_text = font_sm.render("Click in the square near the edge you want to place a line.", True, pearl_assets.WHITE)
+    rules_6_rect = rules_6_text.get_rect()
+    rules_6_rect.center = base_rect_x, base_rect_y + 400
+
+    author_text = font_tiny.render("Developed By: George Kochera", True, pearl_assets.GRAY)
+    author_rect = author_text.get_rect()
+    author_rect.center = ((WIDTH - 110), (HEIGHT - 12))
+
+
+    # apply the text to the screen
+    screen.blit(bground, (0, 0))
+    screen.blit(press_space_text, press_space_rect)
+    screen.blit(title_text, title_rect)
+    screen.blit(reset_text, reset_rect)
+    screen.blit(x_to_quit_text, x_to_quit_rect)
+    screen.blit(press_q_to_quit_text, press_q_to_quit_rect)
+    screen.blit(rules_1_text, rules_1_rect)
+    screen.blit(rules_2_text, rules_2_rect)
+    screen.blit(rules_3_text, rules_3_rect)
+    screen.blit(rules_4_text, rules_4_rect)
+    screen.blit(rules_5_text, rules_5_rect)
+    screen.blit(rules_6_text, rules_6_rect)
+    screen.blit(author_text, author_rect)
+
 """ GAME CODE STARTS HERE"""
 
 # initialize the game
 pygame.init()
 
 # load font
+font_tiny = pygame.font.Font('OpenSans-Semibold.ttf', 14)
+font_sm = pygame.font.Font('OpenSans-Semibold.ttf', 24)
 font_med = pygame.font.Font('OpenSans-Semibold.ttf', 60)
 font_large = pygame.font.Font('OpenSans-Semibold.ttf', 90)
 
@@ -49,28 +127,39 @@ while running:
             # set the game started flag
             game_started = True
 
+        # x to quit to main screen
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_x:
+            game_started = False
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+            running = False
+
         # right mouse click to place a piece
         if event.type == pygame.MOUSEBUTTONDOWN and game_started and not p.game_won:
             position = pygame.mouse.get_pos()
             pearl = p.get_square_from_click(position)
             quadrant = pearl_assets.get_quadrant_from_click(position, pearl)
-
             if p.is_valid_move(pearl, quadrant):
                 p.draw_straight_segment(background, pearl, quadrant)
                 screen.blit(background, (0, 0))
+                p.set_move_constraints()
+                p.game_status()
 
-            # check for a winning condition
-            if p.check_for_winning():
-                text_surface = font_med.render("YOU WON!", True, pearl_assets.BLACK)
-                text_rect = text_surface.get_rect()
-                text_rect.center = ((WIDTH // 2), (HEIGHT // 2))
-                screen.blit(text_surface, text_rect)
+                # check for a winning condition
+                if p.check_for_winning():
+                    text_surface = font_large.render("YOU WON!", True, pearl_assets.BLACK)
+                    text_rect = text_surface.get_rect()
+                    text_rect.center = ((WIDTH // 2), (HEIGHT // 2))
+                    screen.blit(text_surface, text_rect)
+
+                if p.check_for_losing():
+                    text_surface = font_large.render("YOU LOST!", True, pearl_assets.RED)
+                    text_rect = text_surface.get_rect()
+                    text_rect.center = ((WIDTH // 2), (HEIGHT // 2))
+                    screen.blit(text_surface, text_rect)
 
         if not game_started:
-            text_surface = font_med.render("Press Space to Begin", True, pearl_assets.WHITE)
-            text_rect = text_surface.get_rect()
-            text_rect.center = ((WIDTH // 2), (HEIGHT // 2))
-            screen.blit(text_surface, text_rect)
+            splash_screen()
     pygame.display.flip()
 
 pygame.quit()
--- pearl_assets.py
+++ pearl_assets.py
@@ -12,6 +12,7 @@ WIDTH = 700
 HEIGHT = 700
 COLS = 7
 ROWS = 7
+NUM_PEARLS = 4
 COL_WIDTH = WIDTH // COLS
 ROW_HEIGHT = HEIGHT // ROWS
 COL_CENTER = COL_WIDTH // 2
@@ -27,6 +28,7 @@ ANGLED = "Angled"
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
+GRAY = (100, 100, 100)
 
 
 class PearlBox:
@@ -69,13 +71,39 @@ def get_quadrant_from_click(position: tuple, pearl: PearlBox):
 
 class PearlGame:
     def __init__(self):
-        self.last_move = None
+        self.last_direction = None
         self.squares = {}
         self.first_move_made = False
         self.moved_from_pearl = None
         self.moved_to_pearl = None
         self.starting_box = None
         self.game_won = False
+        self.second_previous_move = None
+        self.previous_move = None
+        self.must_turn_after_pearl = False
+        self.pearls_visited = 0
+
+    def game_status(self):
+        print("\nGAME STATUS\n-----------------------")
+        print("Last Direction: {}".format(self.last_direction))
+        print("First Move Made: {}".format(self.first_move_made))
+        print("Moved From: {}\nMoved To: {}".format(self.moved_from_pearl, self.moved_to_pearl))
+        print("Starting Box: {}".format(self.starting_box))
+        print("\nMOVES\n----------------\nPrevious:{}\nSecond Previous:{}".format(self.previous_move, self.second_previous_move))
+        print("Must Turn After Pearl: {}".format(self.must_turn_after_pearl))
+
+    def get_square_from_index(self, index: int):
+        return self.squares[index]
+
+    def get_destination_square_from_direction(self, pearl: PearlBox, direction):
+        if direction == NORTH:
+            return self.get_square_from_index(pearl.index - COLS)
+        elif direction == SOUTH:
+            return self.get_square_from_index(pearl.index + COLS)
+        elif direction == EAST:
+            return self.get_square_from_index(pearl.index + 1)
+        elif direction == WEST:
+            return self.get_square_from_index(pearl.index - 1)
 
     def get_square_from_click(self, position: tuple):
         w, h = position
@@ -96,71 +124,105 @@ class PearlGame:
         if direction == SOUTH and pearl.row == ROWS - 1:
             return False
 
+        # Get the destination pearl for comparisons
+        destination_pearl = self.get_destination_square_from_direction(pearl, direction)
 
+        # Determine the type of move
+        if self.last_direction == EAST or self.last_direction == WEST:
+            if direction == NORTH or direction == SOUTH:
+                type_of_move = ANGLED
+            else:
+                type_of_move = STRAIGHT
+        else:
+            if direction == EAST or direction == WEST:
+                type_of_move = ANGLED
+            else:
+                type_of_move = STRAIGHT
+
+        # Once the first line is placed (we don't care where it starts) we enforce pearl rules...
         if self.first_move_made:
 
-            # Determine the type of move
-            if self.last_move == EAST or self.last_move == WEST:
-                if direction == NORTH or direction == SOUTH:
-                    type_of_move = ANGLED
-                else:
-                    type_of_move = STRAIGHT
-            else:
-                if direction == EAST or direction == WEST:
-                    type_of_move = ANGLED
-                else:
-                    type_of_move = STRAIGHT
-            print(type_of_move)
-
-            # Enforce pearl rules, straight for white ones, and right angles for black ones.
+            # Enforce pearl rules
+            # Enforce straight moves for white pearls, and right angles for black pearls.
             if pearl.color == BLACK and type_of_move != ANGLED:
                 return False
             if pearl.color == WHITE and type_of_move != STRAIGHT:
                 return False
 
-            print(self.moved_to_pearl, pearl.index)
-            # Enforce the line staying continuous
-            if self.moved_to_pearl != pearl.index:
+            # Enforce "doesn't not turn immediately before or after every black pearl"
+            if destination_pearl.color == BLACK and type_of_move == ANGLED:
+                print("BAD MOVE: Trying to turn directly before entering a Black pearl.")
+                return False
+            if self.moved_from_pearl.color == BLACK and type_of_move == ANGLED:
+                print("BAD MOVE: Trying to turn directly after leaving a Black pearl.")
                 return False
 
+            # Enforce "does turn immediately before or after every white pearl"
+            if self.moved_from_pearl.color == WHITE and self.must_turn_after_pearl and type_of_move == STRAIGHT:
+                print("BAD MOVE: Must turn directly before or after a White pearl.")
+                return False
 
+            # Enforce the line staying continuous
+            if self.moved_to_pearl.index != pearl.index:
+                return False
+
+            # Enforce non-intersection of line except to win
+            if destination_pearl.visited and destination_pearl != self.starting_box:
+                return False
+
+            # Record the type of move history if the move was valid
+            self.second_previous_move = self.previous_move
+            self.previous_move = type_of_move
         return True
+
+    def set_move_constraints(self):
+        if self.moved_from_pearl.color == WHITE and self.second_previous_move == STRAIGHT:
+            self.must_turn_after_pearl = True
+        else:
+            self.must_turn_after_pearl = False
 
     def draw_straight_segment(self, surface: pygame.Surface, pearl: PearlBox, direction):
         # Record the pearl that was moved from
-        self.moved_from_pearl = pearl.index
         pearl.visited = True
+        self.moved_from_pearl = pearl
+
+        # If we visited a pearl, not an empty box, note it so we know that all pearls were touched at end of game
+        if pearl.color is not None:
+            self.pearls_visited += 1
 
         # Calculate the line dimensions and determine the pearl that was moved to
         d_x, d_y = pearl.center
         if direction == NORTH:
             d_y -= ROW_HEIGHT
-            self.moved_to_pearl = pearl.index - COLS
+            self.moved_to_pearl = self.get_square_from_index(pearl.index - COLS)
         elif direction == SOUTH:
             d_y += ROW_HEIGHT
-            self.moved_to_pearl = pearl.index + COLS
+            self.moved_to_pearl = self.get_square_from_index(pearl.index + COLS)
         elif direction == EAST:
             d_x += COL_WIDTH
-            self.moved_to_pearl = pearl.index + 1
+            self.moved_to_pearl = self.get_square_from_index(pearl.index + 1)
         elif direction == WEST:
             d_x -= COL_WIDTH
-            self.moved_to_pearl = pearl.index - 1
+            self.moved_to_pearl = self.get_square_from_index(pearl.index - 1)
 
         # Draw the line and set the last move direction
         pygame.draw.line(surface, RED, pearl.center, (d_x, d_y), 3)
-        self.last_move = direction
+        self.last_direction = direction
         if not self.first_move_made:
             self.first_move_made = True
-            self.starting_box = pearl.index
+            self.moved_from_pearl = pearl
+            self.starting_box = pearl
 
     def check_for_winning(self):
-        if self.moved_to_pearl == self.starting_box:
+        if self.moved_to_pearl == self.starting_box and self.pearls_visited == NUM_PEARLS:
             self.game_won = True
             return True
         else:
             return False
 
-
+    def check_for_losing(self):
+        if self.moved_to_pearl == self.starting_box and self.pearls_visited < NUM_PEARLS:
+            return True
 
 
 def new_game(surface: pygame.Surface):
@@ -189,14 +251,22 @@ def new_game(surface: pygame.Surface):
         pygame.draw.line(surface, BLACK, (0, j), (HEIGHT, j), 2)
 
     # set some pearls on the board
-    for k in range(0, 9):
+    pieces_placed_at = []
+    for k in range(0, NUM_PEARLS):
+
+        # prevent pieces from being placed on top of each other
         position = random.randint(0, len(p.squares) - 1)
+        while position in pieces_placed_at:
+            position = random.randint(0, len(p.squares) - 1)
+        pieces_placed_at.append(position)
+
         pearl = p.squares[position]
+        print(k, position)
         if k % 2 == 0:
             draw_pearl(surface, pearl, WHITE)
         else:
             draw_pearl(surface, pearl, BLACK)
-
+    print()
     return p
 
 
@@ -213,3 +283,4 @@ def draw_pearl(surface: pygame.Surface, pearl: PearlBox, color):
         pearl.color = WHITE
     else:
         pearl.color = None
+
